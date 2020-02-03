2eb48c4b643fa3db059294ffa69357f28ed3d7a4
mtkhao <60361180+mtkhao@users.noreply.github.com>
2020-02-03 13:19:26 -0500

    Add files via upload

---
+++ KhoaNguyen.R
@@ -0,0 +1,16 @@
+2017^(1/3)
+abs(5.7-6.8)/.58
+a = 1:12 
+a
+b = c(1,3,5,7,9,11)
+b
+c = seq(1,11,2)
+c
+ln.a = log(a)
+ln.a
+d = c^2
+d
+?sd
+#sd function computes the standard deviation of values in x. If na.rm is TRUE then missing values are removed before computation proceeds.
+Name = "Khoa"
+paste("My name is", Name)
